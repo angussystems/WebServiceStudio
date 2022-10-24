33da72f18edabfa8301412c36b8eaf6e83da4a75
Adam Michaud <Adam.Michaud@mrisoftware.com>
2022-10-24 17:13:33 -0400

    Forces the program to use TLS 1.2.

--- WebServiceStudio/MainForm.cs
+++ WebServiceStudio/MainForm.cs
@@ -86,6 +86,8 @@ namespace WebServiceStudio
 
         public MainForm()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             InitializeComponent();
             wsdl = new Wsdl();
             CheckForIllegalCrossThreadCalls = false;
